4b9c779e6fc6e1f7269aada14a4f8c6f58699e85
Caleb Tutor <cmtutor123@gmail.com>
2023-02-24 14:29:21 -0500

    started implementing grid system

--- randomData2D.py
+++ randomData2D.py
@@ -2,8 +2,13 @@
 
 class RandomTerrain:
 
+    gridDict = {}
+
     def __init__(self):
         pass
 
-    def generateData(self):
-        pass
+    def generateGrid(self, xRadius, yRadius):
+        for x in range(-xRadius, xRadius + 1):
+            for y in range(-yRadius, yRadius + 1):
+                gridDict[(x,y)] = new TerrainSquare()
+    
